e7baa4734e0de01fa412dcf51768817f1c7d7021
SomeDebris <mags2ramsey@gmail.com>
2022-10-13 13:25:28 -0700

    Change format of file
    Add newlines, keep spaces equivalent. no functionality
    added or changed.

--- Gradient_stuff.m
+++ Gradient_stuff.m
@@ -1,16 +1,25 @@
 clc; clear; format compact;
+
+disp('Calculates things to do with Gradients.');
+
 syms x y z
+
 f = input('Give function ')    ;%Function
 fx = diff(f, x)
 fy = diff(f, y)
-fz = diff(f,z)
-p =  input('Give x val ') ;%P(x)
+fz = diff(f, z)
+
+p =  input('Give x val ')  ;%P(x)
 q =  input('Give y val ')  ;%P(y)
 r =  input('Give z val ')  ;%P(z)
 numFx = subs(fx, [x, y, z], [p, q, r])
 numFy = subs(fy, [x, y, z], [p, q, r])
 numFz = subs(fz, [x, y, z], [p, q, r])
+
 rate = norm([numFx numFy numFz])
+
 Ui = input('Give U vector (as vector [a b c] ');
+
 Uf = Ui./norm(Ui)
+
 DIRu = dot([numFx numFy numFz], Uf)
