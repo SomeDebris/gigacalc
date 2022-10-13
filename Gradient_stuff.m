25e67e7daa888545db9a0eea83c7e5c617600a8e
SomeDebris <mags2ramsey@gmail.com>
2022-10-13 16:04:22 -0700

    improve formatting consistency

--- Gradient_stuff.m
+++ Gradient_stuff.m
@@ -4,14 +4,14 @@ disp('Calculates things to do with Gradients.');
 
 syms x y z
 
-f = input('Give function ')    ;%Function
+f  = input('Give function ');%Function
 fx = diff(f, x)
 fy = diff(f, y)
 fz = diff(f, z)
 
-p =  input('Give x val ')  ;%P(x)
-q =  input('Give y val ')  ;%P(y)
-r =  input('Give z val ')  ;%P(z)
+p  = input('Give x val ');  %P(x)
+q  = input('Give y val ');  %P(y)
+r  = input('Give z val ');  %P(z)
 numFx = subs(fx, [x, y, z], [p, q, r])
 numFy = subs(fy, [x, y, z], [p, q, r])
 numFz = subs(fz, [x, y, z], [p, q, r])
