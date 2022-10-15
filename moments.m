0f3175127dd835f525d01411e1591d723621b231
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2022-10-15 16:56:53 -0700

    check input arg count

--- moments.m
+++ moments.m
@@ -1,18 +1,17 @@
 function R = calc_moment(r,F,u)
-    if (norm(u) ~= 1) 
-        disp("Input 'u' is not a unit vector!");
-
-        disp("Changing to:");
-
-        u /= norm(u)
-    end
-    
     M = cross(r,F);
 
-    R = dot(u,M);
-end
+    if exist('u', 'var'),
+        if (norm(u) ~= 1) 
+            disp("Input 'u' is not a unit vector!");
 
-function R = calc_moment(r,F)
-    R = cross(r,F);
+            disp("Changing to:");
+
+            u /= norm(u)
+        end
+
+        R = dot(u,M);
+    else
+        R = M;
 end
 
