c40b73aff308a3e0b73704d69afbccb6908b1cb6
KongKong82 <115742072+KongKong82@users.noreply.github.com>
2022-10-18 00:08:16 -0700

    Update MinMax.m
    Got rid of stuff that wasn't working bc I don't like looking at error messages

--- MinMax.m
+++ MinMax.m
@@ -8,14 +8,10 @@ fx = diff(f(x,y), x); fy = diff(f(x,y), y); fxx = diff(fx, x); fxy = diff(fx, y)
 D(x,y) = fxx.*fyy - fxy.^2 ;
 % Solve for 0's;
 eqns = [fx == 0 , fy ==0];
-[x0, y0] = solve(eqns, [x y])
+[x0, y0] = solve(eqns, [x y]); crit = [x0 y0]
 %The plug
 disp('D(x0, y0): ')
-D = D(x0, y0)
-disp('fxx(x0, y0): ')
-Fxx = subs(fxx, [x y], [x0 y0])
+    D = D(x0, y0)'
 disp('f(x0, y0): ')
-f = f(x0, y0)
-%Graph boi
-[x, y] = meshgrid(-15:.5:15 -15:.5:15);
-mesh(x, y, f(x,y))
+    f = f(x0, y0)'
+
