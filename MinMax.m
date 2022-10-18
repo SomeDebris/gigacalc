f174b373f8b0b92db32caea4b2cb9687caf3d8c0
KongKong82 <115742072+KongKong82@users.noreply.github.com>
2022-10-17 23:35:30 -0700

    Update MinMax.m
    Not important changes for UE

--- MinMax.m
+++ MinMax.m
@@ -5,17 +5,17 @@ f(x,y) = input('Give Function: ');
 % Make the Partials and display them;
 fx = diff(f(x,y), x); fy = diff(f(x,y), y); fxx = diff(fx, x); fxy = diff(fx, y); fyy = diff(fy, y); df = [fx fy fxx fxy fyy]'
 %Get that Daddy D
-D(x,y) = fxx*fyy - fxy^2 ;
+D(x,y) = fxx.*fyy - fxy.^2 ;
 % Solve for 0's;
 eqns = [fx == 0 , fy ==0];
-[x0, y0] = solve(eqns, [x y]);
+[x0, y0] = solve(eqns, [x y])
 %The plug
 disp('D(x0, y0): ')
-D(x0, y0)
+D = D(x0, y0)
 disp('fxx(x0, y0): ')
-subs(fxx, [x y], [x0 y0])
+Fxx = subs(fxx, [x y], [x0 y0])
 disp('f(x0, y0): ')
-f(x0, y0)
+f = f(x0, y0)
 %Graph boi
-[x, y] = meshgrid(-20:.5:20 -20:.5:20);
+[x, y] = meshgrid(-15:.5:15 -15:.5:15);
 mesh(x, y, f(x,y))
