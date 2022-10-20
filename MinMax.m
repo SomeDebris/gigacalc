03afaf96438e11f65cf732ed20f6acb666ac249a
KongKong82 <115742072+KongKong82@users.noreply.github.com>
2022-10-19 19:21:32 -0700

    Update MinMax.m
    Final Form
    unless I want to procrastinate later and make it more user-friendly but it works as a function file now so very cool.

--- MinMax.m
+++ MinMax.m
@@ -1,16 +1,21 @@
-function R = MinMax(f(x,y))
-    syms f(x,y) D(x,y)
+function R = MinMax(pp)
+    syms f(x,y) D(x,y) fxx(x,y)
     assume(x, 'real'); assume(y, 'real');
+    f(x,y) = pp
     % Make the Partials and display them;
-    fx = diff(f(x,y), x); fy = diff(f(x,y), y); fxx = diff(fx, x); fxy = diff(fx, y); fyy = diff(fy, y); df = [fx fy fxx fxy fyy]'
+        fx = diff(f(x,y), x)
+        fy = diff(f(x,y), y)
+        fxx(x,y) = diff(fx, x)
+        fxy = diff(fx, y)
+        fyy = diff(fy, y)
     %Get that Daddy D
-    D(x,y) = fxx.*fyy - fxy.^2 ;
+        D(x,y) = fxx(x,y).*fyy - fxy.^2 ;
     % Solve for 0's;
-    eqns = [fx == 0 , fy ==0];
-    [x0, y0] = solve(eqns, [x y]); crit = [x0 y0]
+        eqns = [fx == 0 , fy ==0];
+        [x0, y0] = solve(eqns, [x y]); crit = [x0 y0]
     %The plug
-    disp('D(x0, y0): ')
-        D = D(x0, y0)'
-    disp('f(x0, y0): ')
-        f = f(x0, y0)'
+    disp('Values at the critical points: ')
+        Dc = D(x0, y0)
+        fc = f(x0, y0)
+        fxxc = fxx(x0,y0)
 end
