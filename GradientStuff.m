79b723408e3da81edfe7af0c7cbf2b6994118904
KongKong82 <115742072+KongKong82@users.noreply.github.com>
2022-10-19 21:07:55 -0700

    Update GradientStuff.m
    Added better functionality

--- GradientStuff.m
+++ GradientStuff.m
@@ -1,15 +1,20 @@
-function r = GradientStuff(f, Ui, P)
+function r = GradientStuff(f, P, Ui)
     syms x y z; format compact;
     fx = diff(f, x)
     fy = diff(f, y)
     fz = diff(f,z)
-    p =  P(1);%P(x)
-    q =  P(2);%P(y)
-    r =  P(3);%P(z)
-    numFx = subs(fx, [x, y, z], [p, q, r])
-    numFy = subs(fy, [x, y, z], [p, q, r])
-    numFz = subs(fz, [x, y, z], [p, q, r])
-    rate = norm([numFx numFy numFz])
-    Uf = Ui./norm(Ui)
-    DIRu = dot([numFx numFy numFz], Uf)
+    if exist('P', 'var'),
+        p =  P(1);%P(x)
+        q =  P(2);%P(y)
+        r =  P(3);%P(z)
+        numFx = subs(fx, [x, y, z], [p, q, r])
+        numFy = subs(fy, [x, y, z], [p, q, r])
+        numFz = subs(fz, [x, y, z], [p, q, r])
+        rate = norm([numFx numFy numFz])
+        if exist('Ui', 'var'),
+            Uf = Ui./norm(Ui)
+            MAGdirU = dot([numFx numFy numFz], Uf)
+        end
+    end
+    disp("I have no clue why there's an 'ans' displayed but you can probably just ignore that :) ")
 end
