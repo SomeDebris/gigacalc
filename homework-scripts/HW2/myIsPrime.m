e746380740f1783fc47a921b351222e29ea62867
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-02-26 21:36:51 -0800

    myIsPrime.m made better

--- homework-scripts/HW2/myIsPrime.m
+++ homework-scripts/HW2/myIsPrime.m
@@ -1,5 +1,15 @@
 %%
 
 function [out] = myIsPrime(n)
-    out = any(primes(n) == n);
+    divide_test = 1:n;
+
+    num_divisible = sum( rem(n,divide_test) == 0 );
+
+    if (num_divisible == 2)
+        out = 1;
+        return;
+    else
+        out = 0;
+        return;
+    end
 end
