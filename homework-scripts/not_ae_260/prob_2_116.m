198b3ef846ca3062bf60140fb438488423fcab01
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-03-13 10:48:46 -0700

    add MORE

--- homework-scripts/not_ae_260/prob_2_116.m
+++ homework-scripts/not_ae_260/prob_2_116.m
@@ -11,10 +11,14 @@ phi = atand( h / ( d - r * cosd(theta)) );
  
 dist_A_to_current = [ d - r * cosd(theta), h ];
 a = v^2 / ( 2 * norm(dist_A_to_current) )
+
 a_r = -a * cosd( phi + theta );
+a_theta = a * sind( phi + theta );
 v_r = -v * cosd( phi + theta );
 v_theta = v * sind( phi + theta );
 
 r_dot = v_r
 theta_dot = v_theta / r
+
+r_dot2 = -a_r + ( r * theta_dot )
 
