158cf19a8604a55e8c02fd5e5b2fd5086c130266
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-03-15 12:53:11 -0700

    add 3/65

---
+++ homework-scripts/not_ae_260/prob_3_65.m
@@ -0,0 +1,23 @@
+%% 3/65
+
+t = 32 * 1000;
+r = 9.6 * 1000;
+v = 3 * 1000;
+g = 6;
+m = 2000;
+theta = 30;
+
+e_t = [ sind( theta ), cosd( theta ) ];
+e_g = [ 0, -1 ];
+g_vec = g * e_g;
+
+R = r * -e_t;
+T = t * e_t;
+wt = m * g_vec;
+
+F_net = R + T + wt;
+a_net = F_net ./ m;
+
+a_n = g * sind(theta)
+
+v^2 / a_n
