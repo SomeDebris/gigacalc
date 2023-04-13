8803b82c000f4a674b4b450f504d9b03682f947a
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-04-12 20:15:06 -0700

    HW4: finish myNewton function

--- homework-scripts/HW4/myNewton.m
+++ homework-scripts/HW4/myNewton.m
@@ -1,4 +1,19 @@
 %% problem 2
 
 function [R,E] = myNewton(f, df, x0, tol)
+    R = [];
+    E = [];
 
+    x_n = x0;
+
+    for i = 1:100000
+        R(i) = x_n;
+        E(i) = abs( f(R(i)) );
+
+        if (E(i) < tol)
+            break;
+        end
+
+        x_n = x_n - (f(x_n) / df(x_n));
+    end
+end
