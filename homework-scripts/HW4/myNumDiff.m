b073dc69716f0290471ba226e3845c6cc388fa40
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-04-12 23:37:40 -0700

    HW4: add prob 5 function

---
+++ homework-scripts/HW4/myNumDiff.m
@@ -0,0 +1,33 @@
+%% problem 5
+
+function [dy, X] = myNumDiff(f, a, b, n, option)
+    x_space = linspace(a,b,n);
+    f_space = f_space;
+    
+    switch (option)
+        case 'forward'
+            dy = diff( f_space );
+
+            X = x_space( 1:(end-1) );
+        case 'backward'
+            dy = diff( f_space );
+
+            X = x_space( 2:(end) );
+        case 'central'
+
+            if (length(f_space) - 2 > 0)
+                print('Can''t compute center derivative! length too small.');
+                return;
+            end
+
+            for i = 1:(length(f_space) - 2)
+                dy(i) = (f_space(i + 2) - f_space(i)) / 2;
+            end
+
+            X = x_space( 2:(end-1) );
+        otherwise
+            print('you didn''t specify an `option`! Please do so.');
+            return;
+    end
+end
+
