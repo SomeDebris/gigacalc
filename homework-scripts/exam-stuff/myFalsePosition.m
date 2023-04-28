c91d180bf37ea723a938ee99e75345dea48dd878
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-04-27 17:42:02 -0700

    exam: add problem done during exam to here

---
+++ homework-scripts/exam-stuff/myFalsePosition.m
@@ -0,0 +1,27 @@
+%% problem 1
+
+function [R, E] = myFalsePosition(f,a,b,tol)
+    R = [];
+
+    m_a = a;
+    m_b = b;
+
+    for i = 1:100000
+        center = (m_a * f(m_b) - m_b * f(m_a)) / (f(m_b) - f(m_a));
+        f_test = f(center);
+        close_test = (m_b - m_a) / 2;
+
+        R(i) = center;
+        E(i) = abs(f( R(i) ));
+
+        if (f_test == 0 || E(i) < tol)
+            break;
+        end
+
+        if ( sign(f_test) == sign(f(m_a)) )
+            m_a = center;
+        else
+            m_b = center;
+        end
+    end
+end
