f427f2d361cae9a1990b7d7bc7be917c8559edcf
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-05 13:41:03 -0700

    myNPrimes: problem 2

---
+++ homework-scripts/exam-stuff/exam_final/myNPrimes.m
@@ -0,0 +1,24 @@
+function [primes] = myNPrimes(N)
+    primes = [];
+    current_number = 0;
+
+    while length(primes) < N
+        current_number++;
+        
+        if (current_number == 1)
+            continue
+        end
+        
+        divisibles = [];
+
+        for i = 1:current_number
+            if (rem(current_number, i) == 0)
+                divisibles(end + 1) = i;
+            end
+        end
+
+        if (length(divisibles) == 2)
+            primes(end + 1) = current_number;
+        end
+    end
+end
