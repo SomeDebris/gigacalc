416cf7df3c8d1fd662970b98d1054a2cb059d3c3
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-05 13:45:08 -0700

    myChebyshevPoly2: problem 3

---
+++ homework-scripts/exam-stuff/exam_final/myChebyshevPoly2.m
@@ -0,0 +1,13 @@
+function [y] = myChebyshevPoly2(n,x)
+  y = [];
+  for i = 1:length(x)
+    if (n == 0)
+      y(end+1) = 1;
+    elseif (n == 1)
+      y(end+1) = 2 * x(i);
+    else
+      y(end+1) = (2 * x(i) * myChebyshevPoly1(n - 1, x(i))) - myChebyshevPoly1(n - 2, x(i));
+    end
+  end
+end
+
